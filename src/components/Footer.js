e5617f750e1211b0980b74f847fb953d5720a05b
ayushv95 <102852613+ayushv95@users.noreply.github.com>
2023-05-02 03:01:26 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Footer(){
+    return (
+        <div className="footer container">
+            <div>
+                <a href="/" className="title mr-1">True Opinion</a>
+            </div>
+            <div>
+                <p className="title">Thank you for visiting our website</p>
+            
+            </div>
+            <div>
+                <p className="title">Contact Us</p>
+                <a href="https://www.linkedin.com/in/ayushv10/" className="li">LinkedIn</a>
+                <a href="https://twitter.com/ayushv25703" className="li">Twitter</a>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
